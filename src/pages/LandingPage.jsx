dbc3138e897fbbfbcd0690c45a7a8a641461e493
Mikayla Mao <m23mao@uwaterloo.ca>
2023-12-23 17:54:09 -0500

    changed landing page bg color, deleted navbar from  template

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -6,8 +6,8 @@ import LandingPageNavbar from "../components/LandingPageNavbar";
 const LandingPage = () => {
   const navigate = useNavigate();
   return (
-    <div className="bg-gray-900">
-      <LandingPageNavbar />
+    <div>
+      
       <section className="mt-24 mx-auto max-w-screen-xl pb-12 px-4 items-center lg:flex md:px-8">
         <div className="space-y-4 flex-1 sm:text-center lg:text-left">
           <h1 className="text-white font-bold text-4xl xl:text-5xl">
