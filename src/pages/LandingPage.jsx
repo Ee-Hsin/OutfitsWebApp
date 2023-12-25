9f9c6cd9f6af6b1b6148004cee5df21c306cda52
ritaxiang <zherita0502@gmail.com>
2023-12-25 12:01:00 -0500

    sign up page ui code

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -35,7 +35,7 @@ const LandingPage = () => {
         </div>
         <div className="flex-1 text-center mt-7 lg:mt-0 lg:ml-3">
           <img
-            src="https://img.freepik.com/premium-vector/hand-drawn-dress-code-illustrated_52683-94768.jpg"
+            src="https://png.pngtree.com/element_pic/17/03/16/d5f5354bb7a6fe02bb4775d55afca27c.png"
             className="w-full mx-auto sm:w-10/12  lg:w-full"
           />
         </div>
