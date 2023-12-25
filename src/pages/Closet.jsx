c114b6d8e2c95e9615b2f8f3684765e4a38b99f5
Mikayla Mao <m23mao@uwaterloo.ca>
2023-12-25 16:32:36 -0500

    select image, display in "upload", no style, image not stored

--- src/pages/Closet.jsx
+++ src/pages/Closet.jsx
@@ -1,17 +1,33 @@
-// import React from "react";
+//import React from "react";
 import { useNavigate } from "react-router"
 import { IoIosAdd } from "react-icons/io"
 
 const Closet = () => {
   const navigate = useNavigate()
 
+  const handleFileInput = (e) => {
+    const file = e.target.files[0];
+
+    if(file){
+      console.log(file);
+      navigate("/app/upload", {state:{file: file}});
+    }
+  }
+
   return (
     <div className="flex justify-between text-white font-montserrat px-36 py-4">
       <div className="border-b-2 border-[#201B21] border-opacity-60 w-[30%] pl-4 pb-4">
         Uploaded items
       </div>
+      <input
+        type="file"
+        id="fileInput"
+        onChange={handleFileInput}
+        accept="image/*"  // only accept image
+        style={{display:"none"}}  // hide default input style
+      />
       <button
-        onClick={() => navigate("/app/upload")}
+        onClick={() => document.getElementById("fileInput").click()}
         className="flex items-center bg-white bg-opacity-40 w-32 pl-8 rounded-3xl shadow-xl hover:bg-opacity-50"
       >
         upload
--- src/pages/Upload.jsx
+++ src/pages/Upload.jsx
@@ -1,10 +1,19 @@
-import React from "react";
+//import React from "react";
+import { useLocation } from "react-router";
 
 
 const Upload = () => {
+  const location = useLocation();
+  const { file } = location.state || {};
+
+  if(!file){    // when file is not available in the state
+    return <div>No image selected</div>
+  }
+
   return(
     <div>
       upload
+      <img src={URL.createObjectURL(file)} alt="selected image"/>
     </div>
   )
 }
