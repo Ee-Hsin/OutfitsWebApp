1d8ef51bbe0264f63d4218d2b1e292cbd16ed8dd
Mikayla Mao <m23mao@uwaterloo.ca>
2023-12-26 02:24:43 -0500

    upload page

--- src/pages/Upload.jsx
+++ src/pages/Upload.jsx
@@ -1,8 +1,10 @@
 //import React from "react";
-import { useLocation } from "react-router";
+import { useLocation, useNavigate } from "react-router";
+import { IoIosArrowBack } from "react-icons/io";
 
 
 const Upload = () => {
+  const navigate = useNavigate();
   const location = useLocation();
   const { file } = location.state || {};
 
@@ -11,9 +13,31 @@ const Upload = () => {
   }
 
   return(
-    <div>
-      upload
-      <img src={URL.createObjectURL(file)} alt="selected image"/>
+    <div >
+      <div className="flex text-white font-montserrat py-4">
+        <button onClick={() => navigate("/app/closet")} className="pl-10 pr-16 text-xl">
+          <IoIosArrowBack />
+        </button>
+        <div className="border-b-2 border-[#201B21] border-opacity-60 w-[26%] pl-4 pb-4">
+          Categorize your item
+        </div>
+      </div>
+      <div className="flex items-center justify-between px-36 py-24">
+        <div className="relative w-96 h-96 bg-white rounded-2xl shadow-3xl">
+          <img 
+            src={URL.createObjectURL(file)} 
+            alt="selected image"
+            className="w-full h-full object-cover rounded-2xl"
+            style={{ objectFit: 'contain' }}
+          />
+        </div>
+        <div>
+          hi
+        </div>
+
+
+
+      </div>
     </div>
   )
 }
