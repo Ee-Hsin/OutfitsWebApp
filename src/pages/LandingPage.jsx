5bd5e3318b1b6ee91d0b67bacda6c378bcc11472
Mikayla Mao <m23mao@uwaterloo.ca>
2023-12-27 19:45:25 -0500

    landing page reactive ui

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -11,16 +11,17 @@ const LandingPage = () => {
   const imageStyle = {
     border: '2px solid #000', // Adjust the border style as needed
     borderRadius: '8px', // Optional: Add border-radius for rounded corners
+    boxShadow: '0 20px 20px rgba(0, 0, 0, 0.1)',
   };
   
   return (
     <div>
       <LandingPageNavbar />
       <section className="mt-24 mx-auto max-w-screen-xl pb-12 px-4 items-center lg:flex md:px-8">
-        <div className="space-y-4 flex-1 sm:text-center lg:text-left">
+        <div className="space-y-4 px-4 flex-1 text-center sm:text-center lg:text-left">
           <h1 className="text-white font-bold text-4xl xl:text-5xl">
             Welcome to
-            <span className="text-indigo-400"> Fitsss!</span>
+            <span className="text-indigo-300"> Fitsss!</span>
           </h1>
           <p className="text-gray-300 max-w-xl leading-relaxed sm:mx-auto lg:ml-0">
             An application designed to elevate your style game and simplify your
@@ -30,7 +31,7 @@ const LandingPage = () => {
           <div className="pt-10 items-center justify-center space-y-3 sm:space-x-6 sm:space-y-0 sm:flex lg:justify-start">
             <Link
               to="/signup"
-              className="px-7 py-3 w-full bg-white text-gray-800 text-center rounded-md shadow-md block sm:w-auto"
+              className="px-7 py-3 w-36 bg-white text-gray-800 text-center rounded-md shadow-md block sm:w-36 mx-auto  sm:mx-0 hover:bg-opacity-80"
               onClick={() => navigate("/signup")}
             >
               Get started
@@ -46,28 +47,28 @@ const LandingPage = () => {
         <div className="flex-1 text-center mt-7 lg:mt-0 lg:ml-3">
           <img
           src={Preview} alt="" style={imageStyle}
-          className="w-full mx-auto sm:w-10/12  lg:w-full"
+          className="w-full mx-auto sm:w-10/12 lg:w-full"
           />
         </div>
       </section>
-      <section className="py-14">
-        <div className="max-w-screen-xl mx-auto md:px-8">
-          <div className="items-center gap-x-12 sm:px-4 md:px-0 lg:flex">
-            <div className="flex-1 sm:hidden lg:block">
+      <section className="mx-auto px-4 py-14">
+        <div className="max-w-screen-xl md:px-8 ">
+          <div className="items-center gap-x-12 sm:px-4 md:px-0 lg:flex space-y-4">
+            <div className="flex-1 text-center mt-7 lg:block lg:mt-0 lg:ml-3">
               <img
                 src={Upload} style={imageStyle}
-                className="md:max-w-lg sm:rounded-lg"
+                className="w-full mx-auto sm:w-10/12 lg:w-full"
                 alt=""
               />
             </div>
-            <div className="max-w-xl px-4 space-y-3 mt-6 sm:px-0 md:mt-0 lg:max-w-2xl">
+            <div className="max-w-screen-xl px-4 space-y-3 mt-6 sm:px-0 md:mt-0 lg:max-w-2xl mx-auto md:px-8">
               <h3 className="text-indigo-300 font-semibold">
                 Professional services
               </h3>
-              <p className="text-white text-3xl font-semibold sm:text-4xl">
+              <p className="text-white text-3xl font-semibold xl:text-4xl">
                 Free outfit organizer with customized clothing recommendations
               </p>
-              <p className="mt-3 text-gray-300">
+              <p className="mt-3 text-gray-300 sm:mx-auto lg:ml-0">
                 Duis aute irure dolor in reprehenderit in voluptate velit esse
                 cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
                 cupidatat non proident, sunt in culpa qui officia deserunt
