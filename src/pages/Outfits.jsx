064556bf18f688e41c7c11f1e75707fd972570be
ritaxiang <zherita0502@gmail.com>
2023-12-29 00:09:27 -0500

    update on outfits page UI

--- src/pages/Outfits.jsx
+++ src/pages/Outfits.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router"
 import { IoIosAdd } from "react-icons/io"
+import { BsHeart } from 'react-icons/bs'
 
 
 const Outfits = () => {
@@ -15,24 +16,21 @@ const Outfits = () => {
     }
   }
 
-  const handleButtonClick = () => {
-    // Handle button click
-  };
-
   const saved = [
     {
         title: "Outfit 1",
-        desc: "Going into this journey, I had a standard therapy regimen, based on looking at the research literature. After I saw the movie, I started to ask other people what they did for their anxiety, and some",
-        img: "https://images.unsplash.com/photo-1556155092-490a1ba16284?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+        desc: "#jeans",
+        img: "https://i.pinimg.com/736x/34/3f/1e/343f1ee9058363a3ee39ee1e03142c5e.jpg",
         authorLogo: "https://api.uifaces.co/our-content/donated/xZ4wg2Xj.jpg",
         authorName: "Sidi dev",
         date: "Jan 4 2022",
         href: "javascript:void(0)"
+
     },
     {
         title: "Outfit 2",
-        desc: "According to him, â€œI'm still surprised that this has happened. But we are surprised because we are so surprised.â€More revelations about Whittington will be featured in the film",
-        img: "https://images.unsplash.com/photo-1620287341056-49a2f1ab2fdc?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+        desc: "#jacket",
+        img: "https://i.ebayimg.com/images/g/yA4AAOSwFglkNOe0/s-l1200.webp",
         authorLogo: "https://api.uifaces.co/our-content/donated/FJkauyEa.jpg",
         authorName: "Micheal",
         date: "Jan 4 2022",
@@ -40,8 +38,8 @@ const Outfits = () => {
     },
     {
         title: "Outfit 3",
-        desc: "#",
-        img: "https://images.unsplash.com/photo-1517694712202-14dd9538aa97?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+        desc: "#shoes",
+        img: "https://fashionista.com/.image/t_share/MTY3MTkyNTM2NzE1MTA5ODI1/uniqlo-u-sweatshirt.jpg",
         authorLogo: "https://randomuser.me/api/portraits/men/46.jpg",
         authorName: "Luis",
         date: "Jan 4 2022",
@@ -49,8 +47,8 @@ const Outfits = () => {
     },
     {
         title: "Outfit 4",
-        desc: "The powerful gravity waves resulting from the impact of the planets' moons â€” four in total â€” were finally resolved in 2015 when gravitational microlensing was used to observe the",
-        img: "https://images.unsplash.com/photo-1617529497471-9218633199c0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80",
+        desc: "#hoodie",
+        img: "https://torontolife.com/wp-content/uploads/2023/11/Canada-Goose-Inline.png",
         authorLogo: "https://api.uifaces.co/our-content/donated/KtCFjlD4.jpg",
         authorName: "Lourin",
         date: "Jan 4 2022",
@@ -94,9 +92,10 @@ const Outfits = () => {
   //   </button>
   // </div>
   <section className="mt-12 mx-auto px-4 max-w-screen-xl md:px-8 font-montserrat text-white">
-    <div className="flex justify-between">
+  <div className="flex items-center justify-between">
+    <div className="flex">
       <button
-        className="border-b-2 border-[#201B21] border-opacity-60 w-[30%] pl-4 pb-4 focus:outline-none mr-5"
+        className="border-b-2 border-[#201B21] border-opacity-60 text-sm px-3 pb-2 focus:outline-none transition duration-300 ease-in-out hover:border-indigo-500 focus:border-indigo-500"
         onClick={() => {
           // Handle favorites button click
         }}
@@ -104,62 +103,73 @@ const Outfits = () => {
         Favorites
       </button>
       <button
-        className="border-b-2 border-[#201B21] border-opacity-60 w-[30%] pl-4 pb-4 focus:outline-none mr-5"
+        className="border-b-2 border-[#201B21] border-opacity-60 text-sm px-3 pb-2 focus:outline-none ml-3 transition duration-300 ease-in-out hover:border-indigo-500 focus:border-indigo-500"
         onClick={() => {
           // Handle suggestion button click
         }}
       >
         Suggestions
       </button>
-      <input
-        type="file"
-        id="fileInput"
-        onChange={handleFileInput}
-        accept="image/*" // only accept image
-        style={{ display: 'none' }} // hide default input style
-      />
-      <button
-        onClick={() => document.getElementById('fileInput').click()}
-        className="flex items-center bg-white bg-opacity-40 w-32 pl-8 rounded-3xl shadow-xl hover:bg-opacity-50"
-      >
-        Upload
-        <IoIosAdd className="text-2xl" />
-      </button>
-
-      <button
-      onClick={handleButtonClick}
-      className="fi fi-ts-circle-heart"
-      // Add any additional styling or attributes as needed
-    ></button>
     </div>
-
-  <div className="mt-12 grid gap-2 sm:grid-cols-2 lg:grid-cols-3">
-      {
-          saved.map((items, key) => (
-              <article className="max-w-md mx-auto mt-4 shadow-lg border rounded-md duration-300 hover:shadow-sm" key={key}>
-                  <a href={items.href}>
-                      <img src={items.img} loading="lazy" alt={items.title}  className="w-full h-48 rounded-t-md" />
-                      <div className="flex items-center mt-2 pt-3 ml-4 mr-2">
-                          <div className="flex-none w-10 h-10 rounded-full">
-                              <img src={items.authorLogo} className="w-full h-full rounded-full" alt={items.authorName} />
-                          </div>
-                          <div className="ml-3">
-                              <span className="block text-gray-900">{items.authorName}</span>
-                              <span className="block text-gray-400 text-sm">{items.date}</span>
-                          </div>
-                      </div>
-                      <div className="pt-3 ml-4 mr-2 mb-3">
-                          <h3 className="text-xl text-gray-900">
-                              {items.title}
-                          </h3>
-                          <p className="text-gray-400 text-sm mt-1">{items.desc}</p>
-                      </div>
-                  </a>
-              </article>
-          ))
-      }
+    <input
+      type="file"
+      id="fileInput"
+      onChange={handleFileInput}
+      accept="image/*" // only accept image
+      style={{ display: 'none' }} // hide default input style
+    />
+    <button
+      onClick={() => document.getElementById('fileInput').click()}
+      className="flex items-center bg-white bg-opacity-40 text-sm px-3 py-2 rounded-3xl shadow-xl hover:bg-opacity-50 focus:outline-none"
+    >
+      Create
+      <IoIosAdd className="text-2xl ml-1" />
+    </button>
   </div>
-</section>
+
+  <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 justify-center mt-8">
+  {saved.map((items, key) => (
+    <article
+      className="relative bg-white bg-opacity-20 w-[300px] h-[450px] mb-8 rounded-2xl shadow-xl overflow-hidden"
+      key={key}
+    >
+      <a href={items.href} className="flex flex-col h-full">
+        <img
+          src={items.img}
+          loading="lazy"
+          alt={items.title}
+          className="w-full h-[240px] bg-white rounded-t-2xl shadow-3xl"
+        />
+        <div className="flex items-center mt-2 pt-3 ml-4 mr-2">
+          <div className="flex-none w-10 h-10 rounded-full">
+            <img
+              src={items.authorLogo}
+              className="w-full h-full rounded-full"
+              alt={items.authorName}
+            />
+          </div>
+          <div className="ml-3">
+            <span className="block text-white-900">{items.authorName}</span>
+            <span className="block text-white-400 text-sm">{items.date}</span>
+          </div>
+        </div>
+        <div className="pt-3 ml-4 mr-2 mb-3">
+          <h3 className="text-xl text-white-700">{items.title}</h3>
+          <p className="text-[#EBEBF5] text-opacity-60">{items.desc}</p>
+        </div>
+        <button
+          className="absolute bottom-4 right-4 flex items-center justify-center w-8 h-8 bg-white bg-opacity-20 rounded-full focus:outline-none"
+          onClick={() => {
+            // Handle heart button click
+          }}
+        >
+          <BsHeart className="text-white-500" />
+        </button>
+      </a>
+    </article>
+  ))}
+</div>
+  </section>
   )
 }
 
