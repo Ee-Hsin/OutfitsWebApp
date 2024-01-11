dcc44dec25b609bb7396847852e9370efa31fba8
Jordan (Ee Hsin) <eehsinkok777@gmail.com>
2024-01-11 18:17:03 -0500

    More typescript migration stuff

--- src/pages/Suggestions.jsx
+++ src/pages/Suggestions.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { BsHeart, BsHeartFill } from "react-icons/bs";
 import { useGetCloset, useGetRecommendations } from "../hooks/query";
