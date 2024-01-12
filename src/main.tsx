c654f3ce6c32a38d595318e14c8775672ed3b771
Jordan (Ee Hsin) <eehsinkok777@gmail.com>
2024-01-11 23:13:35 -0500

    Moved main and api to typescript

--- src/main.tsx
+++ src/main.tsx
@@ -1,23 +1,16 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
-import App from "./App.tsx"
+import App from "./App"
 import "./index.css"
 import { GoogleOAuthProvider } from "@react-oauth/google"
 import { AuthContextProvider } from "./hooks/AuthContext"
 
-ReactDOM.createRoot(document.getElementById("root")).render(
+ReactDOM.createRoot(document.getElementById("root") ?? document.body).render(
   <GoogleOAuthProvider clientId="11190237112-ocdfo045sg481ltiqs28fkcdfsjncufo.apps.googleusercontent.com">
     <React.StrictMode>
       <AuthContextProvider>
-        {/* <ClosetProvider> */}
-        {/* <FavoritesProvider> */}
-      
-            <App />
-            {/* </FavoritesProvider> */}
-     
-        {/* </ClosetProvider> */}
+        <App />
       </AuthContextProvider>
     </React.StrictMode>
-  </GoogleOAuthProvider>,
-  document.getElementById("root")
+  </GoogleOAuthProvider>
 )
