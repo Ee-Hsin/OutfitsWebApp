ce37ec7de19acdedf5ff8ed0db316e42db6a5db1
Jordan (Ee Hsin) <eehsinkok777@gmail.com>
2024-01-11 19:20:48 -0500

    Migrated AuthContext to typescript

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,7 @@ import ReactDOM from "react-dom/client"
 import App from "./App.tsx"
 import "./index.css"
 import { GoogleOAuthProvider } from "@react-oauth/google"
-import { AuthContextProvider } from "./hooks/AuthContext.jsx"
-// import { FavoritesProvider } from "./hooks/FavoritesContext.jsx"
-// import { ClosetProvider } from "./hooks/ClosetContext.jsx"
+import { AuthContextProvider } from "./hooks/AuthContext"
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <GoogleOAuthProvider clientId="11190237112-ocdfo045sg481ltiqs28fkcdfsjncufo.apps.googleusercontent.com">
