e9503f7fbd8a0490bf60f851cd07215d1a848f31
SirMixalotMixalot <58115761+SirMixalotMixalot@users.noreply.github.com>
2024-06-13 17:42:00 -0700

    Update constants.ts

--- src/services/constants.ts
+++ src/services/constants.ts
@@ -3,7 +3,7 @@ import { ClothingSubcategories } from "../types/interfaces";
 export const TESTING = false;
 export const API_URL = TESTING
   ? "http://localhost:8080"
-  : "https://fitss.up.railway.app";
+  : "outfitbackend-production.up.railway.app";
 
 export const COLORS = [
   { value: "black", label: "black" },
